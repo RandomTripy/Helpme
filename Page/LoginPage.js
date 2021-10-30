71b94eab8ebd1dafaff58804e898b83c9057f39d
RandomTripy <firemilost@gmail.com>
2021-10-31 01:42:21 +0900

    fifth commit

--- Page/LoginPage.js
+++ Page/LoginPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, Button} from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
+import main from '../assets/main.jpg'
 
 
 
@@ -13,6 +14,7 @@ export default function LoginPage(navigation, route) {
     */
     <ScrollView style={styles.container}>
         
+        <Image style={styles.mainImage} source={main}/>
         
         <TextInput
           style={styles.textFormTop}
@@ -48,6 +50,8 @@ const styles = StyleSheet.create({
   container: {
     //앱의 배경 색
     backgroundColor: 'brown',
+    paddingTop:40,
+    paddingHorizontal:130,
     
   },
   loginButtonText: {
@@ -59,6 +63,7 @@ const styles = StyleSheet.create({
     marginTop:1,
 	    //왼쪽 공간으로 부터 이격'
     textAlign: 'center',
+    
   },
   loginButton: {
     width:150,
@@ -70,8 +75,16 @@ const styles = StyleSheet.create({
   textFormTop: {
     backgroundColor :'white',
     borderWidth:1,
-    width:100,
+    width:150,
     height:50,
+    borderRadius:15,
+    textAlign:'center',
+    marginHorizontal:1,
+  },
+  mainImage: {
+    width:150,
+    height:90,
+
   }
   
 });
